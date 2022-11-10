48a6c17d37bd5805e828cff974d5430ecc5c0064
Willian Schlichting <wschlichting@1worldsync.com>
2022-11-09 22:20:19 -0300

    widgets class

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,36 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Container(color: Colors.red,width: 100, height: 100,),
-          Container(color: Colors.blue,width: 50, height: 50,)
-        ],
-      ),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:[
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.red,width: 100, height: 100,),
+              Container(color: Colors.blue,width: 50, height: 50,)
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.blue,width: 100, height: 100,),
+              Container(color: Colors.red,width: 50, height: 50,)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.cyan,width: 50, height: 50,),
+              Container(color: Colors.pinkAccent,width: 50, height: 50,),
+              Container(color: Colors.purple,width: 50, height: 50,)
+            ],
+          )
+
+        ]
+      )
     );
   }
 }
